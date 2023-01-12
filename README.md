9d13f21474b5cae50b4add8c0b8dcd9266dec9db
mehmetsencer <53850382+mehmetsencer@users.noreply.github.com>
2023-01-12 15:48:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-simple-get-users-current-location
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-simple-get-users-current-location-tse6sz)
---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+  public lat;
+  public lng;
+
+  public ngOnInit(): void {
+    this.getLocation();
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position: Position) => {
+        if (position) {
+          console.log("Latitude: " + position.coords.latitude +
+            "Longitude: " + position.coords.longitude);
+          this.lat = position.coords.latitude;
+          this.lng = position.coords.longitude;
+          console.log(this.lat);
+          console.log(this.lat);
+        }
+      },
+        (error: PositionError) => console.log(error));
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+}
